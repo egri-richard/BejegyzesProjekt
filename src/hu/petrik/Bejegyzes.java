49bf1ec8abe62f36489c9ed557f905f7cded17df
Your Name <you@example.com>
2021-09-29 15:02:07 +0200

    class functions finished 1. feladat kesz

--- src/hu/petrik/Bejegyzes.java
+++ src/hu/petrik/Bejegyzes.java
@@ -17,4 +17,46 @@ public class Bejegyzes {
         this.letrejott = LocalDateTime.now();
         this.szerkesztve = LocalDateTime.now();
     }
+
+    public String getSzerzo() {
+        return this.szerzo;
+    }
+
+    public String getTartalom() {
+        return this.tartalom;
+    }
+
+    public void setTartalom(String in) {
+        this.tartalom = in;
+    }
+
+    public Integer getLikeok() {
+        return this.likeok;
+    }
+
+    public LocalDateTime getLetrejott() {
+        return this.letrejott;
+    }
+
+    public LocalDateTime getSzerkesztve() {
+        return this.szerkesztve;
+    }
+
+    public void plusLike() {
+        this.likeok++;
+    }
+
+    @Override
+    public String toString() {
+        String retString;
+        if (this.szerkesztve == this.letrejott) {
+            retString = this.szerzo +" - "+ this.likeok +" - "+this.letrejott +"\n"+
+                        this.tartalom;
+        } else {
+            retString = this.szerzo +" - "+ this.likeok +" - "+this.letrejott +"\n"+
+                        "Szerkesztve: " + this.szerkesztve +"\n"+
+                        this.tartalom;
+        }
+        return retString;
+    }
 }
