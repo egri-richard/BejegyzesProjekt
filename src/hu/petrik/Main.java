8cbfbec1e349f3f3e7ee8bb2998c3290905d4ffc
Your Name <you@example.com>
2021-09-29 15:39:04 +0200

    can assign the list*20 random likes

--- src/hu/petrik/Main.java
+++ src/hu/petrik/Main.java
@@ -1,17 +1,25 @@
 package hu.petrik;
 
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     static List<Bejegyzes> Blist = new ArrayList<>();
     static Scanner scan = new Scanner(System.in);
+    static Random rnd = new Random();
 
     public static void main(String[] args) {
 	    add2();
 	    addCustom();
+	    addFromFile();
+	    assignLikes();
         System.out.println(Blist);
     }
 
@@ -34,6 +42,31 @@ public class Main {
             tartTemp = scan.next();
 
             Blist.add(new Bejegyzes(szerzoTemp, tartTemp));
+        }
+    }
+
+    public static void addFromFile() {
+        try {
+            FileReader file = new FileReader("bejegyzesek.txt");
+            BufferedReader read = new BufferedReader(file);
+            String line = read.readLine();
+
+            while(line != null) {
+                String[] elements = line.split(";");
+                Blist.add(new Bejegyzes(elements[0], elements[1]));
+                line = read.readLine();
+            }
+
+            read.close();
+            file.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void assignLikes() {
+        for (int i = 0; i < (Blist.size() * 20); i++) {
+            Blist.get(rnd.nextInt(Blist.size())).plusLike();
         }
     }
 }
