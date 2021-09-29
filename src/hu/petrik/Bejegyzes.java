92822d04f871d81ae5a73259bd6efdff51dc1afd
Your Name <you@example.com>
2021-09-29 14:50:59 +0200

    class data added, constructor created

--- src/hu/petrik/Bejegyzes.java
+++ src/hu/petrik/Bejegyzes.java
@@ -1,4 +1,20 @@
 package hu.petrik;
 
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+
 public class Bejegyzes {
+    private String szerzo;
+    private String tartalom;
+    private Integer likeok;
+    private LocalDateTime letrejott;
+    private LocalDateTime szerkesztve;
+
+    Bejegyzes(String szerzo, String tartalom) {
+        this.szerzo = szerzo;
+        this.tartalom = tartalom;
+        this.likeok = 0;
+        this.letrejott = LocalDateTime.now();
+        this.szerkesztve = LocalDateTime.now();
+    }
 }
